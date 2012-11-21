617cbdc2ca9db373dda53582293237bd97638705
PengAn <pengan1987@msn.com>
2012-11-21 11:43:46 -0700

    initial version

---
+++ utresdefCalc/MainForm.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace utresdefCalc
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.hVisiable = new System.Windows.Forms.TextBox();
+            this.hSyncStart = new System.Windows.Forms.TextBox();
+            this.hSyncEnd = new System.Windows.Forms.TextBox();
+            this.hTotal = new System.Windows.Forms.TextBox();
+            this.vVisiable = new System.Windows.Forms.TextBox();
+            this.vSyncStart = new System.Windows.Forms.TextBox();
+            this.vSyncEnd = new System.Windows.Forms.TextBox();
+            this.vTotal = new System.Windows.Forms.TextBox();
+            this.pixelFreq = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxUT = new System.Windows.Forms.TextBox();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ModeLine to Sun Ray utresdef";
+            // 
+            // hVisiable
+            // 
+            this.hVisiable.Location = new System.Drawing.Point(40, 96);
+            this.hVisiable.Name = "hVisiable";
+            this.hVisiable.Size = new System.Drawing.Size(100, 20);
+            this.hVisiable.TabIndex = 2;
+            // 
+            // hSyncStart
+            // 
+            this.hSyncStart.Location = new System.Drawing.Point(146, 96);
+            this.hSyncStart.Name = "hSyncStart";
+            this.hSyncStart.Size = new System.Drawing.Size(100, 20);
+            this.hSyncStart.TabIndex = 3;
+            // 
+            // hSyncEnd
+            // 
+            this.hSyncEnd.Location = new System.Drawing.Point(252, 96);
+            this.hSyncEnd.Name = "hSyncEnd";
+            this.hSyncEnd.Size = new System.Drawing.Size(100, 20);
+            this.hSyncEnd.TabIndex = 4;
+            // 
+            // hTotal
+            // 
+            this.hTotal.Location = new System.Drawing.Point(358, 96);
+            this.hTotal.Name = "hTotal";
+            this.hTotal.Size = new System.Drawing.Size(100, 20);
+            this.hTotal.TabIndex = 5;
+            // 
+            // vVisiable
+            // 
+            this.vVisiable.Location = new System.Drawing.Point(40, 122);
+            this.vVisiable.Name = "vVisiable";
+            this.vVisiable.Size = new System.Drawing.Size(100, 20);
+            this.vVisiable.TabIndex = 6;
+            // 
+            // vSyncStart
+            // 
+            this.vSyncStart.Location = new System.Drawing.Point(146, 122);
+            this.vSyncStart.Name = "vSyncStart";
+            this.vSyncStart.Size = new System.Drawing.Size(100, 20);
+            this.vSyncStart.TabIndex = 7;
+            // 
+            // vSyncEnd
+            // 
+            this.vSyncEnd.Location = new System.Drawing.Point(252, 122);
+            this.vSyncEnd.Name = "vSyncEnd";
+            this.vSyncEnd.Size = new System.Drawing.Size(100, 20);
+            this.vSyncEnd.TabIndex = 8;
+            // 
+            // vTotal
+            // 
+            this.vTotal.Location = new System.Drawing.Point(358, 122);
+            this.vTotal.Name = "vTotal";
+            this.vTotal.Size = new System.Drawing.Size(100, 20);
+            this.vTotal.TabIndex = 9;
+            // 
+            // pixelFreq
+            // 
+            this.pixelFreq.Location = new System.Drawing.Point(40, 55);
+            this.pixelFreq.Name = "pixelFreq";
+            this.pixelFreq.Size = new System.Drawing.Size(100, 20);
+            this.pixelFreq.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Visiable";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SyncStart";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(249, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "SyncEnd";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(355, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Total";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(36, 39);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 13);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "PixelClock";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "H";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 125);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "V";
+            // 
+            // textBoxUT
+            // 
+            this.textBoxUT.Location = new System.Drawing.Point(16, 158);
+            this.textBoxUT.Multiline = true;
+            this.textBoxUT.Name = "textBoxUT";
+            this.textBoxUT.Size = new System.Drawing.Size(147, 214);
+            this.textBoxUT.TabIndex = 10;
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(207, 241);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalc.TabIndex = 11;
+            this.buttonCalc.Text = "Calc";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(311, 241);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 12;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 385);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.textBoxUT);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pixelFreq);
+            this.Controls.Add(this.vTotal);
+            this.Controls.Add(this.vSyncEnd);
+            this.Controls.Add(this.vSyncStart);
+            this.Controls.Add(this.vVisiable);
+            this.Controls.Add(this.hTotal);
+            this.Controls.Add(this.hSyncEnd);
+            this.Controls.Add(this.hSyncStart);
+            this.Controls.Add(this.hVisiable);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "utresdefGen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox hVisiable;
+        private System.Windows.Forms.TextBox hSyncStart;
+        private System.Windows.Forms.TextBox hSyncEnd;
+        private System.Windows.Forms.TextBox hTotal;
+        private System.Windows.Forms.TextBox vVisiable;
+        private System.Windows.Forms.TextBox vSyncStart;
+        private System.Windows.Forms.TextBox vSyncEnd;
+        private System.Windows.Forms.TextBox vTotal;
+        private System.Windows.Forms.TextBox pixelFreq;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxUT;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ utresdefCalc/MainForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace utresdefCalc
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double _pixelFreq = Convert.ToDouble(this.pixelFreq.Text);
+                int _hVisible = Convert.ToInt32(this.hVisiable.Text);
+                int _hSyncStart = Convert.ToInt32(this.hSyncStart.Text);
+                int _hSyncEnd = Convert.ToInt32(this.hSyncEnd.Text);
+                int _hTotal = Convert.ToInt32(this.hTotal.Text);
+                int _hFrontPorch = _hSyncStart - _hVisible;
+                int _hBackPorch = _hTotal - _hSyncEnd;
+                int _hSyncWidth = _hSyncEnd - _hSyncStart;
+
+
+                int _vVisible = Convert.ToInt32(this.vVisiable.Text);
+                int _vSyncStart = Convert.ToInt32(this.vSyncStart.Text);
+                int _vSyncEnd = Convert.ToInt32(this.vSyncEnd.Text);
+                int _vTotal = Convert.ToInt32(this.vTotal.Text);
+
+                int _vFrontPorch = _vSyncStart - _vVisible;
+                int _vBackPorch = _vTotal - _vSyncEnd;
+                int _vSyncWidth = _vSyncEnd - _vSyncStart;
+
+                double _refreshRate = _pixelFreq * 1000000 / _vTotal / _hTotal;
+
+                string utresdef = "";
+                utresdef += ("htotal=" + _hTotal.ToString() + "\r\n");
+                utresdef += ("hfp=" + _hFrontPorch.ToString() + "\r\n");
+                utresdef += ("hsyncwidth=" + _hSyncWidth.ToString() + "\r\n");
+                utresdef += ("vtotal=" + _vTotal.ToString() + "\r\n");
+                utresdef += ("vfp=" + _vFrontPorch.ToString() + "\r\n");
+                utresdef += ("vsyncwidth=" + _vSyncWidth.ToString() + "\r\n");
+                utresdef += ("vcomposite=8" + "\r\n");
+                utresdef += ("pixclock=" + (_pixelFreq * 100).ToString() + "\r\n");
+                utresdef += ("xres=" + _hVisible.ToString() + "\r\n");
+                utresdef += ("yres=" + _vVisible.ToString() + "\r\n");
+                utresdef += ("hz=" + Math.Round(_refreshRate).ToString() + "\r\n");
+
+
+                textBoxUT.Text = utresdef;
+            }
+            catch(Exception ex){
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in Controls)
+            {
+                if (c is TextBox)
+                {
+                    c.Text = "";
+                }
+            }
+        }
+
+    
+    }
+}
